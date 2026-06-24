88a3c542d5e86f4d7d97d6c7cae9061d3e1b3aa5
UniformStrawn <jpeg0703@gmail.com>
2026-06-24 15:58:29 -0300

    Update Cardapio.java

--- Cardapio.java
+++ Cardapio.java
@@ -1,4 +1,21 @@
-public class Cardapio {
+public class Cardapio{
+private Pratos[] vetPratos; 
+private int index; 
+
+public Cardapio(int quantidadePratos){
+    this.index = 0;
+    this.vetPratos = new Prats[quantidadePratos];
+
+} 
+public boolean adicionarPrato(Prato prato){
+    if(index<vetPratos.length){
+        vetPratos[index]=prato;
+        index++;
+        return true;
+    }
+    return false; 
+}
+
 
 
 
