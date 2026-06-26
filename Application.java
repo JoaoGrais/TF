b2aca60f02c8c36becf43508b14b74757eaee3fe
UniformStrawn <jpeg0703@gmail.com>
2026-06-25 22:25:52 -0300

    Update Application.java

--- Application.java
+++ Application.java
@@ -17,15 +17,5 @@ public class Application {
 
 
     }
-}
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args){
-        Scanner teclado = new Scanner(System.in);
-
-
-
-
-    }
 }
