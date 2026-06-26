aa9433dfc04616317938ba26ec376673aad5c83b
UniformStrawn <jpeg0703@gmail.com>
2026-06-25 22:26:26 -0300

    Update Prato.java

--- Prato.java
+++ Prato.java
@@ -1,8 +1,8 @@
 public class Prato {
-    String nomePrato;
-    double valor;
-    int index;
-    Ingredientes[] vetIngredientes;
+    private String nomePrato;
+    private double valor;
+    private int index;
+    private Ingredientes[] vetIngredientes;
 
     public Prato(String nomePrato, double valor, int quantidadeIngrediente){
         this.nomePrato = nomePrato;
@@ -10,7 +10,24 @@ public class Prato {
         this.vetIngredientes = new Ingredientes [quantidadeIngrediente];
         index = 0;
     }
-
+    public void setNome(String nomePrato){
+        this.nomePrato = nomePrato;
+    }
+    public void setValor(double valor){
+        this.valor=valor;
+    }
+    public void setQuantidadeIngrediente(int quantidadeIngrediente){
+        this.vetIngredientes =new Ingredientes[quantidadeIngrediente];
+    }
+    public String getNome(){
+        return nomePrato;
+    }
+    public double getValor(){
+        return valor;
+    }
+    public Ingredientes[] getVetIngredientes(){
+        return vetIngredientes;
+    }
     public boolean adicionarIngrediente(Ingredientes ingredientes){
         if (index < vetIngredientes.length){
             vetIngredientes[index] = ingredientes;
@@ -20,19 +37,31 @@ public class Prato {
             return false;
         }
     }
-
+    public boolean removerIngrediente(String nomeIngrediente){
+       for(int i =0; i<index; i++){
+           if(vetIngredientes[i].getNome().equalsIgnoreCase(nomeIngrediente)){
+             for(int j =i; j<index-1; j++){ //começar em i pois é onde o ingrediente a ser removido está
+               vetIngredientes[j] = vetIngredientes[j+1]; //tirou o ingrediente anterior
+               
+             }
+             index--;
+             vetIngredientes[index] = null; 
+             return true; 
+           }
+        }
+        return false; 
+    }
     public Ingredientes consultarIngrediente(String nome){
         for (int i = 0; i < index; i++) {
-            if (vetIngredientes[i].getNome().equals(nome)){
+            if (vetIngredientes[i].getNome().equalsIgnoreCase(nome)){
                 return vetIngredientes[i];
             }
         }
         return null;
     }
-
     public int buscarPosicao(String nome) {
         for (int i = 0; i < index; i++) {
-            if (vetIngredientes[i].getNome().equals(nome)) {
+            if (vetIngredientes[i].getNome().equalsIgnoreCase(nome)) {
                 return i;
             }
         }
