d08f515152cab438fec55c9124baa72961c5c906
JoaoGrais <115734114+JoaoGrais@users.noreply.github.com>
2026-06-25 21:07:21 -0300

    Update Ingredientes.java

--- Ingredientes.java
+++ Ingredientes.java
@@ -1,19 +1,23 @@
 public class Ingredientes {
-    String nome;
-    double quantidade;
+    private String nome;
+    private double quantidade;
+    private String medida;
 
-    public Ingredientes(String nome, double quantidade){
+    public Ingredientes(String nome, double quantidade, String medida){
         this.nome = nome;
         this.quantidade = quantidade;
+        this.medida = medida;
     }
 
     public void setNome(String nome) {this.nome = nome;}
     public void setQuantidade(double quantidade) {this.quantidade = quantidade;}
+    public void setMedida(String medida) {this.medida = medida;}
 
     public String getNome() {return nome;}
     public double getQuantidade() {return quantidade;}
+    public String getMedida() {return medida;}
 
     public String toString() {
-        return ("\n o nome do ingrediente é " + nome + "\n e a quantidade " + quantidade);
+        return ("\n o nome do ingrediente � " + nome + "\n e a quantidade " + quantidade + "\n medida " + medida);
     }
 }
