7ecd8049c0bff47902c0919c4a4b3aeee569bd85
UniformStrawn <jpeg0703@gmail.com>
2026-06-25 22:24:29 -0300

    Update Cardapio.java

--- Cardapio.java
+++ Cardapio.java
@@ -1,10 +1,10 @@
 public class Cardapio{
-private Pratos[] vetPratos; 
+private Prato[] vetPratos; 
 private int index; 
 
 public Cardapio(int quantidadePratos){
     this.index = 0;
-    this.vetPratos = new Prats[quantidadePratos];
+    this.vetPratos = new Prato[quantidadePratos];
 
 } 
 public boolean adicionarPrato(Prato prato){
@@ -15,6 +15,26 @@ public boolean adicionarPrato(Prato prato){
     }
     return false; 
 }
+public boolean removerPrato(String nomePrato){
+    for(int i =0; i<index; i++){
+     if(vetPratos[i].getNome().equalsIgnoreCase(nomePrato)){
+       for(int j =i; j<index-1; j++){ //começar em i pois é onde o ingrediente a ser removido está
+          vetPratos[j] = vetPratos[j+1]; //tirou o ingrediente anterior
+             
+       }
+       index--;
+       vetPratos[index] = null; 
+       return true; 
+     }
+    }
+    return false; 
+} 
+public Prato buscarPratoPorNome(String nomePrato){}
+public int buscarPosicao(Prato prato){}
+public Prato[] buscarPratoIngrediente(String nomePrato){}
+public double buscarValorCardapio(){}
+public Prato buscarPratoEconomico(){}
+public void mostrarPratos(){}
 
 
 
