b31faf22efcf931b0b4f88345e40ed27aa3a0d54
UniformStrawn <jpeg0703@gmail.com>
2026-06-25 22:25:35 -0300

    Update and rename Main.java to Application.java

---
+++ Application.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        /**
+        1 – Adicionar prato no cardápio
+        2 – Remover prato do cardápio
+        3 – Consultar cardápio
+        4 – Adicionar ingrediente no prato
+        5 – Remover ingrediente do prato
+        6 – Prato mais econômico
+        7 – Pratos que contém um determinado ingrediente
+        8 – VOCÊ DEFINE E IMPLEMENTA ESTA FUNCIONALIDADE
+        10 – Sair do programa
+        */
+
+
+    }
+}
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+
+
+
+
+    }
+}
